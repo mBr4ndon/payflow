8c153a61a9b17df511f77fb107d23172bbb920e5
Márcio Brandão <brandao.marcio.xix@gmail.com>
2021-08-19 15:49:14 +0100

    Criação de novos screens

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 
+import 'modules/home/home_page.dart';
+import 'modules/login/login_page.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -15,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: AppColors.primary,
       ),
-      home: SplashPage(),
+      home: LoginPage(),
     );
   }
 }
